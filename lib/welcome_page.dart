4946b47324ba2720e6eada71038d5b300b3785a1
HITMAN <101077891+VHITMAN2025@users.noreply.github.com>
2025-03-12 13:04:43 +0530

    Update welcome_page.dart

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
 import 'document_scanner_page.dart';
+import 'details.dart';
 
-class WelcomePage extends StatefulWidget {
-  const WelcomePage({Key? key}) : super(key: key);
-
-  @override
-  _WelcomePageState createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage> {
-  File? _imagefile;
-
-  Future<void> pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedFile != null) {
-      setState(() {
-        _imagefile = File(pickedFile.path);
-      });
-    }
-  }
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,28 +14,32 @@ class _WelcomePageState extends State<WelcomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              'Scan the Voter ID Card',
+              'Welcome to VoteShield!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () async {
-                final result = await Navigator.push(
+              onPressed: () {
+                Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const DocumentScannerPage(),
                   ),
                 );
-
-                if (result != null && result is String) {
-                  setState(() {
-                    _imagefile = File(result);
-                  });
-                  debugPrint('Received file path: ${_imagefile!.path}');
-                  // TODO: Handle the scanned document file
-                }
               },
               child: const Text('Scan Document'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfilePage(),
+                  ),
+                );
+              },
+              child: const Text('Go to Details'),
             ),
           ],
         ),
