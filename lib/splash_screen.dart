cb757c55c21b8a98374e6fd1de4f8a3695466d43
VHITMAN2025 <vijjuroxxx143@gmail.com>
2025-03-27 02:18:53 +0530

    Ad

---
+++ lib/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:voteshield/main.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginUI()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo.png', // Replace with your logo image path
+              width: 500,
+              height: 500,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'VoteShield',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
